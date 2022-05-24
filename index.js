e7380bb60f62ca07cc45147654c5db324180c8ca
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-24 13:25:43 +0600

    Alhamdulillah get API data created for all services

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const cors = require('cors');
@@ -10,6 +11,34 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json())
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zgfd4.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        console.log('DB connected to doctors portal')
+
+        // db collection 
+        const serviceCollection = client.db("doctors_portal").collection("service");
+
+        // get all service from DB and send to client 
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+    }
+    finally {
+
+    }
+
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Doctors Portal Server is running')
